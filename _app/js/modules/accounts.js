a4be3e4498b312206899debd69edd85a0c65bd2b
Eirik Orkelbog <111751379+EirikOrkelbog@users.noreply.github.com>
2024-01-15 21:23:22 +0100

    added function that loops through movements array and updates html

--- _app/js/modules/accounts.js
+++ _app/js/modules/accounts.js
@@ -1,8 +1,28 @@
 export default function () {
+	const containerMovements = document.querySelector('.movements');
+
 	const account1 = {
 		owner: 'Eirik Orkelbog',
 		movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
 		interestRate: 1.2,
 		pin: 1111,
 	};
+
+	const displayMovements = function (movements) {
+		containerMovements.innerHTML = '';
+
+		movements.forEach(function (movement, index) {
+			const type = movement > 0 ? 'deposit' : 'withdrawal';
+
+			const html = `
+			<div class="movements__row">
+				<div class="movements__type movements__type--${type}">${index + 1} ${type}</div>
+				<div class="movements__value">${movement}$</div>
+			</div>
+			`;
+
+			containerMovements.insertAdjacentHTML("afterbegin", html);
+		});
+	};
+	displayMovements(account1.movements);
 }
