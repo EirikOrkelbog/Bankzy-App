7a5f815251a5535b802c226043517260e17ae886
Eirik Orkelbog <111751379+EirikOrkelbog@users.noreply.github.com>
2024-01-16 23:36:36 +0100

    added more accounts

--- _app/js/modules/accounts.js
+++ _app/js/modules/accounts.js
@@ -8,6 +8,27 @@ export default function () {
 		pin: 1111,
 	};
 
+	const account2 = {
+		owner: 'Jessica Davis',
+		movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
+		interestRate: 1.5,
+		pin: 2222,
+	};
+
+	const account3 = {
+		owner: 'Steven Thomas Williams',
+		movements: [200, -200, 340, -300, -20, 50, 400, -460],
+		interestRate: 0.7,
+		pin: 3333,
+	};
+
+	const account4 = {
+		owner: 'Sarah Smith',
+		movements: [430, 1000, 700, 50, 90],
+		interestRate: 1,
+		pin: 4444,
+	};
+
 	const displayMovements = function (movements) {
 		containerMovements.innerHTML = '';
 
