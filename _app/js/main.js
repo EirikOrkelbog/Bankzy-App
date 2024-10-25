7fed1ca658bd60f285ef04a184a64f01cf3a31ea
Eirik Orkelbog <111751379+EirikOrkelbog@users.noreply.github.com>
2024-10-25 20:14:31 +0200

    created function to delete accounts

--- _app/js/main.js
+++ _app/js/main.js
@@ -1,7 +1,9 @@
 import login from "./modules/login.js";
 import usernames from "./modules/usernames.js";
 import transfers from "./modules/transfers.js";
+import closeAccount from "./modules/closeAccount.js";
 
 login();
 usernames();
 transfers();
+closeAccount();
---
+++ _app/js/modules/closeAccount.js
@@ -0,0 +1,32 @@
+import { accounts } from "./data.js";
+import { getCurrentAccount, setCurrentAccount } from './state.js';
+
+const closeButton = document.querySelector('.form__button--close');
+const closeUser = document.querySelector('.form__input--user');
+const closePin = document.querySelector('.form__input--pin');
+const containerApp = document.querySelector('.app');
+const labelWelcome = document.querySelector('.welcome');
+
+export default function closeAccount() {
+	closeButton.addEventListener('click', (e) => {
+		e.preventDefault();
+		let currentAccount = getCurrentAccount();
+
+		if (
+			currentAccount.username === closeUser.value &&
+			currentAccount.pin === Number(closePin.value)
+		) {
+			const index = accounts.findIndex(
+				account => account.username === currentAccount.username
+			);
+
+			accounts.splice(index, 1);
+
+			closeUser.value = closePin.value = '';
+
+			containerApp.style.opacity = 0;
+
+			labelWelcome.textContent = 'Log in to get started';
+		}
+	})
+}
