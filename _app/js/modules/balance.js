954824f5929456411dcf6b1bb4f80254168cbcb4
Eirik Orkelbog <111751379+EirikOrkelbog@users.noreply.github.com>
2024-10-25 19:26:21 +0200

    updated function to use account

--- _app/js/modules/balance.js
+++ _app/js/modules/balance.js
@@ -1,8 +1,8 @@
 const labelBalance = document.querySelector('.balance__value');
 
 // Function for calculating balance based on transactions
-const calculateBalance = function (movements) {
-	const balance = movements.reduce((accumulator, movement) => accumulator + movement, 0);
-	labelBalance.textContent = `${balance}$`;
+const calculateBalance = function (account) {
+	account.balance = account.movements.reduce((accumulator, movement) => accumulator + movement, 0);
+	labelBalance.textContent = `${account.balance}$`;
 };
 export default calculateBalance;
