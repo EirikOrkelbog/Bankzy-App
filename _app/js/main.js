2dc1fa200dc3e8deadafe64ab3b0b24a3ff9271e
Eirik Orkelbog <111751379+EirikOrkelbog@users.noreply.github.com>
2024-10-28 20:26:14 +0100

    added function to request loan

--- _app/js/main.js
+++ _app/js/main.js
@@ -2,8 +2,10 @@ import login from "./modules/login.js";
 import usernames from "./modules/usernames.js";
 import transfers from "./modules/transfers.js";
 import closeAccount from "./modules/closeAccount.js";
+import requestLoan from "./modules/requestLoan.js";
 
 login();
 usernames();
 transfers();
 closeAccount();
+requestLoan();
---
+++ _app/js/modules/requestLoan.js
@@ -0,0 +1,19 @@
+import { getCurrentAccount } from "./state.js";
+import { updateUI } from "./login.js";
+
+const loanAmount = document.querySelector('.form__input--loan-amount');
+const loanButton = document.querySelector('.form__button--loan');
+
+export default function requestLoan() {
+	loanButton.addEventListener('click', (e) => {
+		e.preventDefault();
+		let currentAccount = getCurrentAccount();
+		const amount = Number(loanAmount.value);
+
+		if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
+			currentAccount.movements.push(amount);
+			updateUI(currentAccount);
+		}
+		loanAmount.value = '';
+	})
+}
