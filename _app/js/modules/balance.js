11875e7fa5a65c953e223975788e959b11321662
Eirik Orkelbog <111751379+EirikOrkelbog@users.noreply.github.com>
2024-11-26 19:28:13 +0100

    adding current date

--- _app/js/modules/balance.js
+++ _app/js/modules/balance.js
@@ -1,8 +1,12 @@
 const labelBalance = document.querySelector('.balance__value');
+const date = document.querySelector('.date');
+
+const currentDate = new Date().toLocaleDateString();
+date.textContent = currentDate;
 
 // Function for calculating balance based on transactions
 const calculateBalance = function (account) {
 	account.balance = account.movements.reduce((accumulator, movement) => accumulator + movement, 0);
-	labelBalance.textContent = `${account.balance}$`;
+	labelBalance.textContent = `${account.balance.toFixed(2)}$`;
 };
 export default calculateBalance;
